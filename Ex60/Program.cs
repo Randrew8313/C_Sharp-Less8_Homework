5be7d5b212b9813f3215e703b5dc976f694b41e3
Андрей Репринцев <Randrew8313@gmail.com>
2023-09-16 21:27:39 +0300

    задача 60

--- Ex60/Program.cs
+++ Ex60/Program.cs
@@ -6,3 +6,69 @@
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1) */
 
+
+void GetRandArray3d(int[,,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(2); k++)
+            {
+                int num = new Random().Next(10, 100);
+                if(CheckArray3d(array, num))
+                    {
+                    array[i, j, k] = num;
+                    }    
+            }
+        }
+    }
+}
+bool CheckArray3d(int[,,] array, int num)
+{
+int ind = 0;
+bool res = true;    
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(2); k++)
+            {
+                if(array[i, j, k] == num)
+                {
+                    ind ++;
+                }
+
+            }
+        }
+    }
+    if(ind == 0)
+    {
+        res = true;
+    }
+    return res;
+}
+
+void PrintdArray3d(int[,,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+        Console.WriteLine(); 
+        }
+    }
+}
+
+int x = 2;
+int y = 2;
+int z = 2;
+
+int[,,] array = new int[x, y, z];
+
+GetRandArray3d(array);
+PrintdArray3d(array);
